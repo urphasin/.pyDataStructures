fc9229d79627660a19c633e9603ad8cb518fbc6d
Otito Amuga <urphasin@gmail.com>
2026-02-04 11:36:31 -0700

    tree nodes

---
+++ Structures/TreeDataStructure.py
@@ -0,0 +1,50 @@
+class CompleteBinaryTreeNode:
+    pass
+
+
+class BSTNode:
+    pass
+
+
+class BTreeNode:
+    pass
+
+
+class BPlusNode:
+    pass
+
+
+class PerfectBinaryTreeNode:
+    pass
+
+
+class FullBinaryTreeNode:
+    pass
+
+
+class RedBlackTreeNode:
+    pass
+
+
+class SkewedTreeNode:
+    pass
+
+
+class BalancedBinaryTreeNode:
+    pass
+
+
+class DegenerateBinaryTreeNode:
+    pass
+
+
+
+class UnbalancedBinaryTreeNode:
+    pass
+
+
+
+
+class AVLTreeNode:
+    pass
+
